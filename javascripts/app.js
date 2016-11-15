5a89e8f34df38269b8d221f261b398326286c208
blairvansant <blairbot@gmail.com>
2016-11-14 21:37:09 -0600

    almost working angular todo app

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,44 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+	$scope.navItems =[{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+})
+
+app.controller("TodoCtrl", function($scope){
+	$scope.welcome = "hello";
+	$scope.showListView = true;
+	$scope.items = [
+	{
+		id:0,
+		task:"mow the lawn",
+		isCompleted: true,
+		assignedTo: "Blair"
+	},
+	{
+		id:1,
+		task:"mow the lawn2",
+		isCompleted: false,
+		assignedTo: "Blair"
+	},
+	{
+		id:2,
+		task:"mow the lawn3",
+		isCompleted: false,
+		assignedTo: "Blair"
+	}
+	];
 
 
+	$scope.allItems = function(){
+		console.log("you clicked all items")
+			$scope.showListView = true;
+
+	}
+	$scope.newItem = function(){
+		console.log("you clicked new items")
+			$scope.showListView = false;
+
+	}
+})
