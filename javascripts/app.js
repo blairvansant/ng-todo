2c27bae6b687bd0fc7318880f5b07552ed3875bb
blairvansant <blairbot@gmail.com>
2016-11-15 19:01:04 -0600

    added search function and icons

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,9 +6,12 @@ app.controller("NavCtrl", function($scope){
 	$scope.navItems =[{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
 })
 
+
+
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
 	$scope.showListView = true;
+	$scope.newTask = {};
 	$scope.items = [
 	{
 		id:0,
@@ -40,5 +43,14 @@ app.controller("TodoCtrl", function($scope){
 		console.log("you clicked new items")
 			$scope.showListView = false;
 
+	}
+
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		$scope.newTask.id = $scope.items.length;
+		console.log("new Task in function", $scope.newTask);
+		$scope.items.push($scope.newTask);
+		$scope.newTask ={};
+		$scope.showListView ="true";
 	}
 })
