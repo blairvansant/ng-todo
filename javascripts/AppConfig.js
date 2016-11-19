d92d7eb5874e549145202d9aec5baf1b6d819f5e
blairvansant <blairbot@gmail.com>
2016-11-19 11:38:42 -0600

    added routes

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,4 +1,18 @@
 "use strict"
+
 app.run(function(FIREBASE_CONFIG){
     firebase.initializeApp(FIREBASE_CONFIG);
 });
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/items/list', {
+			templateUrl: 'partials/item-list.html',
+			controller: 'ItemListCtrl'
+		})
+		.when('/items/new', {
+			templateUrl: 'partials/item-new.html',
+			controller: 'ItemNewCtrl'
+		})
+		.otherwise('/items/list')
+})
